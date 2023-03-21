e3212292f8bafb90b4ceaed4e3aa54eeaefd723f
Silasaraujo <77333741+Silovisk@users.noreply.github.com>
2023-03-20 22:50:02 -0300

    .

---
+++ 01 -Estrutura Sequencial/Texto 1 .py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://wiki.python.org.br/EstruturaSequencial"
+
+# fazer uma solicitação HTTP ao URL do link e obter seu conteúdo
+response = requests.get(url)
+content = response.text
+
+# escrever o conteúdo no arquivo README.md
+with open("Texto 1.md", "w") as f:
+    f.write(content)
--- 01 -Estrutura Sequencial/ex018 - tempo aprox em dowload min.py
+++ 01 -Estrutura Sequencial/ex018 - tempo aprox em dowload min.py
@@ -1,4 +1,4 @@
 arquivo = float(input("Informe do tamanho do arquivo em MegaByte: "))
 link = float(input("Informe a velocidade do link em Mbps: "))
 tempo = ((arquivo * 8) / link) / 60
-print (f"O tempo aproximado de download é de {tempo}} minutos")
+print (f"O tempo aproximado de download é de {tempo} minutos")
